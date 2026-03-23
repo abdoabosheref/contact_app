9c2676039e8a1ef9bf763d2906073b751701e6b9
Abdo <abdoabosheref@gmail.com>
2026-03-24 01:33:41 +0200

    feat: current profile delete button

--- lib/core/ui/home_page/widget/custome_grid_view.dart
+++ lib/core/ui/home_page/widget/custome_grid_view.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
-class CustomeGridView extends StatelessWidget {
+import '../../../widgets/user_profile.dart';
+
+class CustomeGridView extends StatefulWidget {
 
    List<Widget> list ;
-   CustomeGridView({required this.list});
 
+
+   CustomeGridView({required this.list,});
+
+  @override
+  State<CustomeGridView> createState() => _CustomeGridViewState();
+}
+
+class _CustomeGridViewState extends State<CustomeGridView> {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      itemCount: list.length,
+      itemCount: widget.list.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount (crossAxisCount: 2,
         mainAxisSpacing: 10,
         childAspectRatio: 9/15,
         crossAxisSpacing: 10,),
       itemBuilder: (context, index) {
-        return list[index] ;
+        return widget.list[index] ;
+
       },);
   }
 }
